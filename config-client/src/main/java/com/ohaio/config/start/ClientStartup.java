3a583bb7203cf5fe09c2e97080ea262ccf335b0e
gaoxiaomeng <gaoxiaomeng@globalegrow.com>
2018-12-18 11:01:23 +0800

    update "./gitignore"

--- config-client/src/main/java/com/ohaio/config/start/ClientStartup.java
+++ config-client/src/main/java/com/ohaio/config/start/ClientStartup.java
@@ -1,5 +1,9 @@
 package com.ohaio.config.start;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 @SpringBootApplication
 
 public class ClientStartup {
--- config-server/src/main/java/com/ohaio/config/start/ConfigServerStartup.java
+++ config-server/src/main/java/com/ohaio/config/start/ConfigServerStartup.java
@@ -1,5 +1,8 @@
 package com.ohaio.config.start;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ConfigServerStartup {
 
